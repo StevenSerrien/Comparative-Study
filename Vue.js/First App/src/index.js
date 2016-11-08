96d444a96a4fba5ef52f1171d49919791f2103d4
StevenSerrien <steven.serrien@student.kdg.be>
2016-11-08 18:38:53 +0100

    Call to API + view on index

--- Vue.js/First App/src/index.js
+++ Vue.js/First App/src/index.js
@@ -3,9 +3,9 @@
 import Vue from 'vue'
 import App from './components/App.vue'
 import Home from './components/Home.vue'
-import SecretQuote from './components/SecretQuote.vue'
-import Signup from './components/Signup.vue'
-import Login from './components/Login.vue'
+// import SecretQuote from './components/SecretQuote.vue'
+// import Signup from './components/Signup.vue'
+// import Login from './components/Login.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
@@ -17,16 +17,16 @@ export var router = new VueRouter()
 router.map({
   '/home': {
     component: Home
-  },
-  'secretquote': {
-    component: SecretQuote
-  },
-  '/login': {
-    component: Login
-  },
-  '/signup': {
-    component: Signup
   }
+  // 'secretquote': {
+  //   component: SecretQuote
+  // },
+  // '/login': {
+  //   component: Login
+  // },
+  // '/signup': {
+  //   component: Signup
+  // }
 })
 
 // Redirect to the home route if any routes are unmatched
