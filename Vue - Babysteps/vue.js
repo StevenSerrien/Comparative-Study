b8823124af040c71a509b45d98882fd0eb804f0d
StevenSerrien <steven.serrien@student.kdg.be>
2016-11-08 16:22:25 +0100

    Composing with components

--- Vue - Babysteps/vue.js
+++ Vue - Babysteps/vue.js
@@ -42,14 +42,30 @@
 //   }
 // })
 
-var app6 = new Vue({
-  el: '#app-6',
+// var app6 = new Vue({
+//   el: '#app-6',
+//   data: {
+//     message: 'Hello Vue.js!'
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
+// })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
   data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
   }
 })
