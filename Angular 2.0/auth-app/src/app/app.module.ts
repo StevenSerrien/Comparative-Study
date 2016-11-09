3348727a605ce0134dfe73cb2198cd395d929719
StevenSerrien <steven.serrien@student.kdg.be>
2016-11-09 10:38:44 +0100

    Routing defined

--- Angular 2.0/auth-app/src/app/app.module.ts
+++ Angular 2.0/auth-app/src/app/app.module.ts
@@ -1,8 +1,18 @@
+// Import dependencies
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AUTH_PROVIDERS } from 'angular2-jwt';
+
+
+// import { AuthGuard } from './common/auth.guard';
+// import { Home } from './home';
+// import { Login } from './login';
+// import { Signup } from './signup';
+// import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +22,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // RouterModule.forRoot(routes, {
+    //   useHash: true
+    // })
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
