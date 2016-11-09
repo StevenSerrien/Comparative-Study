57a0d9a7d41fa6802a0e97b535ff480e38d8e6ce
StevenSerrien <steven.serrien@student.kdg.be>
2016-11-09 13:09:14 +0100

    Working example. the end

--- Angular 2.0/auth-app/src/app/home/home.component.ts
+++ Angular 2.0/auth-app/src/app/home/home.component.ts
@@ -19,12 +19,45 @@ export class HomeComponent implements OnInit {
   constructor(public router: Router, public http: Http, public authHttp: AuthHttp, public jwtHelper: JwtHelper) {
 
     this.jwt = localStorage.getItem('id_token');
-    this.decodedJwt = this.jwt && jwtHelper.decodeToken(this.jwt);
+    this.decodedJwt = jwtHelper.decodeToken(this.jwt);
     console.log(this.decodedJwt);
     console.log(jwtHelper.decodeToken(this.jwt));
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    this.router.navigate(['login']);
+  }
+
+  callAnonymousApi() {
+    this._callApi('Anonymous', 'http://localhost:3001/api/random-quote');
+  }
+
+  callSecuredApi() {
+    this._callApi('Secured', 'http://localhost:3001/api/protected/random-quote');
+  }
+
+  _callApi(type, url) {
+    this.response = null;
+    if (type === 'Anonymous') {
+      // For non-protected routes, just use Http
+      this.http.get(url)
+        .subscribe(
+          response => this.response = response.text(),
+          error => this.response = error.text()
+        );
+    }
+    if (type === 'Secured') {
+      // For protected routes, use AuthHttp
+      this.authHttp.get(url)
+        .subscribe(
+          response => this.response = response.text(),
+          error => this.response = error.text()
+        );
+    }
   }
 
 }
